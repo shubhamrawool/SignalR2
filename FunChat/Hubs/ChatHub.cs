bfa7bb12ea0c827e59094459c20e76ecb72960e9
shubhamrawool <rawool.prasad@gmail.com>
2020-06-01 16:07:49 +0530

    adding sample project

---
+++ FunChat/Hubs/ChatHub.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace WebApplication1.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void JoinRoom(string room)
+        {
+            Groups.Add(Context.ConnectionId, room);
+        }
+        public void SendMessage(string room, string message, int x, int y)
+        {
+            var username = Context.User.Identity.Name;
+            Clients.Group(room).onNewMessage(username, message, x, y);
+        }
+
+        public void SendPrivateMessage(string toUsername, string message)
+        {
+            var fromUsername = Context.User.Identity.Name;
+            Clients.Group("users/" + toUsername).onNewPrivateMessage(fromUsername, message);
+        }
+
+        public override Task OnConnected()
+        {
+            var username = Context.User.Identity.Name;
+            if (!string.IsNullOrEmpty(username))
+            {
+                Groups.Add(Context.ConnectionId, "users/" + username);
+            }
+            return base.OnConnected();
+        }
+    }
+}
---
+++ FunChat/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FunChat.Startup))]
+namespace FunChat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            app.MapSignalR();
+        }
+    }
+}
